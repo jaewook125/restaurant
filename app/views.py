c07c34d6b352e66524ffa65ec897c6d1980d6e6f
jaewook125 <sjw0484@naver.com>
2017-04-14 13:57:08 +0900

    게시판이 랜덤으로 바뀜

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Restaurant
+from django.core import serializers
+json_serializer = serializers.get_serializer("json")()
+companies = json_serializer.serialize(Restaurant.objects.all().order_by('id')[:5], ensure_ascii=False)
 
 def rest_index(request):
-    restaurant = Restaurant.objects.all()
+    restaurant = Restaurant.objects.order_by('?')
     return render(request, 'app/rest_index.html',{
         'restaurant':restaurant
     })
