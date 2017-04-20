f02dbde6db0f70b15c641f1edff66fa4fc5a1e94
jaewook125 <sjw0484@naver.com>
2017-04-20 23:45:44 +0900

    jq moblie 제거, boot3 사용

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Restaurant
 
 class RestForm(forms.ModelForm):
+    restname = forms.CharField(label='restname')
     class Meta:
         model = Restaurant
         fields = ('restimage','restname','restsite',)
